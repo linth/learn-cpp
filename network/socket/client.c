dcd0af2695a81de269ed26c7f42133aa95521efc
linth <zhua0404@gmail.com>
2022-11-27 20:57:17 +0800

    add server-client.

---
+++ network/socket/client.c
@@ -0,0 +1,34 @@
+// /**
+//  * @file client.c
+//  * @author your name (you@domain.com)
+//  * @brief 
+//  * @version 0.1
+//  * @date 2022-11-26
+//  * 
+//  * @copyright Copyright (c) 2022
+//  * 
+//  * Reference:
+//  *  - https://www.geeksforgeeks.org/socket-programming-cc/?ref=lbp
+//  */
+
+// #ifndef __WIN32__  
+//     // for windows.
+//     #include <Winsock2.h>
+// #else
+//     // for linux.
+//     #include <arpa/inet.h> 
+//     #include <sys/socket.h>
+// #endif
+// #include <io.h>
+// // #include <unistd.h>
+// #include <stdio.h>
+// #include <string.h>
+
+// #define PORT 8080
+
+// int main(int argc, char const *argv[]) {
+    
+//     return 0;
+// }
+
+
---
+++ network/socket/server.c
@@ -0,0 +1,42 @@
+// /**
+//  * @file server.c
+//  * @author george
+//  * @brief 
+//  * @version 0.1
+//  * @date 2022-11-26
+//  * 
+//  * @copyright Copyright (c) 2022
+//  * 
+//  * 
+//  * Reference:
+//  *  - https://www.geeksforgeeks.org/socket-programming-cc/
+//  */
+
+// #include <netinet/in.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+// #ifndef __WIN32__  
+//     // for windows.
+//     #include <Winsock2.h>
+// #else
+//     // for linux.
+//     #include <arpa/inet.h> 
+//     #include <sys/socket.h>
+// #endif
+
+// #define PORT 8080
+
+
+// int main(int argc, char const *argv[]) {
+    
+//     int server_fd, new_socket, valread;
+//     struct sockaddr_in address;
+//     int opt = 1;
+//     int addrlen = sizeof(address);
+//     char buffer[1024] = {0};
+//     char* hello = "Hello from server.";
+
+//     return 0;
+// }

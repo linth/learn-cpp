697b590c11e151f6589e6a0f2a9d9261961f258c
linth <zhua0404@gmail.com>
2022-11-28 21:58:44 +0800

    add comments.

--- function/C-library/message-queue-ex.cpp
+++ function/C-library/message-queue-ex.cpp
@@ -8,4 +8,14 @@
  * 
  * Reference:
  *  - https://www.twblogs.net/a/5c263ac1bd9eee16b4a80bb8
+ *  - https://www.geeksforgeeks.org/ipc-using-message-queues/
 */
+
+
+/**
+ * ftok(): is use to generate a unique key.
+ * msgget(): either returns the message queue identifier for a newly created message queue or returns the identifiers for a queue which exists with the same key value.
+ * msgsnd(): Data is placed on to a message queue by calling msgsnd().
+ * msgrcv(): messages are retrieved from a queue.
+ * msgctl(): It performs various operations on a queue. Generally it is use to destroy message queue.
+ */

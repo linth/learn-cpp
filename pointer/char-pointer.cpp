4a52c27e4e7010a2bfcb5cb55b7b9d3b15f1204e
gerogelin <gerogelin@oringnet.com>
2022-12-07 11:37:22 +0800

    update char pointer.

--- pointer/char-pointer.cpp
+++ pointer/char-pointer.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 int main(int argc, char const *argv[]) {
-  
+  //! 初始化指標很重要!
   const char* pstr = "hello world.";
   const char* pstr2 = "good job.";
   const char* pstr3 = "This is a pen.";
@@ -17,12 +17,17 @@ int main(int argc, char const *argv[]) {
   cout << pstr4 << endl; // Hi, everyone.
   cout << pstr5 << endl; // good to know.
 
-  cout << pstr << endl;
+  cout << "pstr: " << pstr << " memory address: " << &pstr << endl;
   pstr = 0;
   
   pstr = pstr2;
   cout << "pstr: " << pstr << "; first address of pointer: " << *pstr << "; value: " << &pstr << endl; // pstr: good job.; first address of pointer: g; value: 0x89611ff858
 
+  string words = "job";
+  cout << words << endl;
+
+  char* firstPointer;
+  cout << &firstPointer << endl; // i.e., 0x3e89bffc00
 
   return 0;
 }

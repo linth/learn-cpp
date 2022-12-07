ec82cb40e8159594886e602b8add7b7d400d1f00
gerogelin <gerogelin@oringnet.com>
2022-12-07 11:36:00 +0800

    add for-loop example.

---
+++ for-loop/infinite-for-loop-base.cpp
@@ -0,0 +1,17 @@
+/**
+ * infinite loop
+ * 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  //! no stop this for-loop.
+  for (;;) {
+    cout << "hi" << endl;
+  }
+  return 0;
+}
+

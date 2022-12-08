023242960b098f44e5169e5f6bc1b3fa1e27e02b
gerogelin <gerogelin@oringnet.com>
2022-12-08 11:22:51 +0800

    add base structue.

---
+++ structure/base.h
@@ -0,0 +1,33 @@
+/**
+ * Basic structure
+ * 
+ * 
+*/
+#include <iostream>
+// #include <cstdlib>
+// #include <stdio.h>
+#include <string>
+
+using namespace std;
+
+// struct mydata {
+//   string name;
+//   string id;
+//   int math;
+//   int eng;
+// } student;
+
+
+int main(int argc, char const *argv[]) {
+  // student.name = "george";
+  // student.math = 90;
+
+  // mydata('george', 'f0001', 90, 100);
+  // cout << "student: " << student.math << endl;
+  // cout << "hello" << endl;
+  printf("hello world.");
+
+  return 0; 
+}
+
+
